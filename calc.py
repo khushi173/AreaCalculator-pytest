17eeecadba0d71eb92eb60dd430488248d97213c
khushi173 <khushijamwal173@gmail.com>
2022-03-28 15:58:17 +0530

    updated

--- calc.py
+++ calc.py
@@ -11,25 +11,25 @@ if __name__ == "__main__":
     print("1.Area of rectangle")
     print("2.Perimeter of rectangle")
     print("3.Area of square")
-    selection = int(input("Enter"))
+    choice = int(input("Enter"))
 
-    while selection < 4:
+    while choice < 4:
 
-        if (selection == 1):
+        if (choice == 1):
             length = int(input("Enter length of rectangle"))
             breadth = int(input("Enter Breadth of rectangle"))
             Areaofrectangle = areaofrectangle(length, breadth)
             print(Areaofrectangle)
             break;
 
-        elif (selection == 2):
+        elif (choice == 2):
             length = int(input("Enter length of rectangle"))
             breadth = int(input("Enter Breadth of rectangle"))
             Perimeterofrectangle = primeterofrectangle(length, breadth)
             print(Perimeterofrectangle)
             break;
 
-        elif (selection == 3):
+        elif (choice == 3):
             side = int(input("Enter the side"))
             Areaofsquare = areaofsquare(side)
             print(Areaofsquare)
